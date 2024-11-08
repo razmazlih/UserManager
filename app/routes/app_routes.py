bebe1a5bbd6bffc0f32092e5b9b0229527e3acac
Raz <retraz123@gmail.com>
2024-11-08 11:03:23 +0200

    add document

--- app/routes/app_routes.py
+++ app/routes/app_routes.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 app_routes = Blueprint('app_routes', __name__)
 
 @app_routes.route('/')
+def admin_index():
+    return render_template("index.html")
+
+@app_routes.route('/admin')
 def index():
-    return "Welcome to the User Management Service"
+    return render_template("admin.html")
